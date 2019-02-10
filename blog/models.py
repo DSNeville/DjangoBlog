434917391b9bcc553c408618d4f6568546889868
DSNeville <jpneville@gmail.com>
2019-02-10 21:55:58 +0000

    web blog framework

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,15 @@ from django.db import models
 class Blog(models.Model):
 
     title = models.CharField(max_length=100)
-    pub_date = models.TimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now=True)
     postimage = models.ImageField(upload_to='images/', default='')
     blogpost = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.blogpost[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
